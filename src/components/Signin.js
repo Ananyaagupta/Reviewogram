12bfd5c8f474ae531b951cb2c3d173b3c4b162e0
Ananyaagupta <sonyguptashikohabad@gmail.com>
2022-02-01 01:46:13 +0530

    logo

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Signin.css';
 import vector from "../components/images/vector1.png";
+import logo from "../components/images/logo.png";
 function Signin() {
   return <div>
       <div className='body'>
       <img className='vector1' src={vector}/>  
+      <img className='logo' src={logo}/>  
       <input type="text" placeholder="Your name here" id="user_name" className="text-camp" name="name"></input>
+
 
       </div>
 
